4f9d1a3cffc4e83945b3b4f8d6c0ba55530d1a81
Julian Rojas <julianrojasgeo@gmail.com>
2023-04-10 14:11:02 -0500

    Item catalogue progress
    Container to show specific lists implemented

--- src/app/components/items/item-list/item-list.component.ts
+++ src/app/components/items/item-list/item-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item.model';
+import { ItemsApiService } from 'src/app/services/itemsapi.service';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss']
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit {
+
+  l_items: Item[] = [];
+  total: number = this.l_items.length;
+  @Input() category: string = "";
+
+  constructor(private service: ItemsApiService) { }
+
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe({
+      next: (items) => {
+        this.l_items = items.filter(
+          (item: { category: string; }) => item.category===this.category
+          ),
+          console.log(items)
+        this.total = this.l_items.length;
+      },
+      error: (console.log),
+      complete: (console.log)
+    })
+  }
 
 }
